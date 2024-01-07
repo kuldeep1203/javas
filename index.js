2d4e5adb0d9d3116b7c8e374417dd557779bd5f3
blaz3fox <kuldeeppawar427@gmail.com>
2024-01-08 03:32:53 +0530

    h

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.send("hello world")
 })
 
+app.post('/conversation\s', (req, res) => {
+    console.log(res.headers["authorizations"])
+    res.send("hello world")
+})
+ 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
 })
